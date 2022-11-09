3afc1b8f9127f2b6e1b549f1f4a4e6566e98d893
Majed-Salah <91992451+Majed-Salah@users.noreply.github.com>
2022-11-08 22:31:54 -0700

    Initial commit

---
+++ src/main/java/edu/ucdenver/pa1_tourney/HelloController.java
@@ -0,0 +1,14 @@
+package edu.ucdenver.pa1_tourney;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
